e4fff778ad69bfb3af17ad41e69e4a06b71da84c
Sulayman <muhammadsulaymannkhan@gmail.com>
2021-09-05 17:36:01 +0500

    Upload section added

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -147,16 +147,13 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
-      {/* <div id='footer'>
+      <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
+            Hiring Predictor made for hackfest 2021
           </p>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,15 +1,92 @@
+import { useState } from 'react'
+
+const initialState = {
+  message: '',
+}
 export const Services = (props) => {
+  const [{ message }, setState] = useState(initialState)
+  const[filename,setFilename]= useState('')
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setState((prevState) => ({ ...prevState, [name]: value }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log( message)
+  }
+  const handleselectedFile= (e)=>{
+    setFilename(e.target.files[0].name)
+  }
+
   return (
-    <div id='services' className='text-center'>
-      <div className='container'>
-        <div className='section-title'>
-          <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
-        </div>
-        <div className='row'>
+    <div>
+      <div id='contact'>
+        <div className='container'>
+          <div className='col-md-8'>
+            <div className='row'>
+              <div className='section-title'>
+                <h2>Upload CV in PDF</h2>
+              </div>
+              <form name='sentMessage' validate onSubmit={handleSubmit}>
+                <div className='row'>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <label className='btn btn-custom btn-lg'>
+                        Choose File
+                          <input type="file" style={{display:'none'}} accept=".pdf" onChange={handleselectedFile} />
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <p>{filename}</p>
+                <div className='form-group'>
+                  <textarea
+                    name='message'
+                    id='message'
+                    className='form-control'
+                    rows='4'
+                    placeholder='comments'
+                    required
+                    onChange={handleChange}
+                  ></textarea>
+                  <p className='help-block text-danger'></p>
+                </div>
+                <div id='success'></div>
+                <button type='submit' className='btn btn-custom btn-lg'>
+                  Process
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className='col-md-3 col-md-offset-1 contact-info'>
+            <div className='contact-item'>
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className='fa fa-map-marker'></i> Address
+                </span>
+                {props.data ? props.data.address : 'loading'}
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-phone'></i> Phone
+                </span>{' '}
+                {props.data ? props.data.phone : 'loading'}
+              </p>
+            </div>
+            <div className='contact-item'>
+              <p>
+                <span>
+                  <i className='fa fa-envelope-o'></i> Email
+                </span>{' '}
+                {props.data ? props.data.email : 'loading'}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
